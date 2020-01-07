ee6c2b19230bf505fa0b028ced4a4bc6bd6e5a54
leejunhwi <kajundrama@gmail.com>
2020-01-07 23:02:46 +0900

    Initial commit

---
+++ src/main/java/me/kajundrama/demospringbootsimpleweb/DemoSpringbootSimpleWebApplication.java
@@ -0,0 +1,13 @@
+package me.kajundrama.demospringbootsimpleweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringbootSimpleWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringbootSimpleWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/kajundrama/demospringbootsimpleweb/DemoSpringbootSimpleWebApplicationTests.java
@@ -0,0 +1,13 @@
+package me.kajundrama.demospringbootsimpleweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringbootSimpleWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
