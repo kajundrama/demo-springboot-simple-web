ca2e8053cb5fb7c05f0285b0871c60f9d3c8de1f
leejunhwi <kajundrama@gmail.com>
2020-01-07 23:11:52 +0900

    [bugfix]패키지 수정

--- src/main/java/me/kajundrama/demospringbootsimpleweb/greeting/GreetingResource.java
+++ src/main/java/me/kajundrama/demospringbootsimpleweb/greeting/GreetingResource.java
@@ -1,4 +1,4 @@
-package me.kajundrama.springbootwebexample.greeting;
+package me.kajundrama.demospringbootsimpleweb.greeting;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
